db024d900b66eba9114a980d67ce82c81311fb8c
GuilhemROLLAND <guilhem99.rolland@gmail.com>
2022-07-13 11:45:48 +0200

    fix valid one cycle

--- A-Eye_FPGA/conv_v2/cmp_add_3_clk.vhd
+++ A-Eye_FPGA/conv_v2/cmp_add_3_clk.vhd
@@ -7,7 +7,7 @@ ENTITY cmp_add_3_clk IS
     PORT (
         clk, rst : IN STD_LOGIC;
         in_data : IN float32;
-        out_data : INOUT float32;
+        out_data : OUT float32;
         in_valid : IN STD_LOGIC;
         out_valid : OUT STD_LOGIC
     );
@@ -15,28 +15,34 @@ END ENTITY;
 
 ARCHITECTURE rtl OF cmp_add_3_clk IS
     SIGNAL cnt : INTEGER := 0;
+    SIGNAL out_data_reg : float32;
+    SIGNAL out_valid_reg : STD_LOGIC;
 BEGIN
+    out_data <= out_data_reg;
+    out_valid <= out_valid_reg;
+
     compute_proc : PROCESS (clk)
     BEGIN
         IF (rising_edge(clk)) THEN
             IF (rst = '0') THEN
-                out_data <= to_float(0.0);
-                out_valid <= '0';
+                out_data_reg <= to_float(0.0);
+                out_valid_reg <= '0';
             ELSE
+                IF (out_valid_reg = '1') THEN
+                    out_valid_reg <= '0';
+                END IF;
                 IF (in_valid = '1') THEN
                     -- internal counter
                     cnt <= (cnt + 1) MOD 3;
                     -- res_data
                     IF (cnt = 0) THEN
-                        out_data <= in_data;
+                        out_data_reg <= in_data;
                     ELSE
-                        out_data <= in_data + out_data;
+                        out_data_reg <= in_data + out_data_reg;
                     END IF;
                     -- res_valid
-                    IF (cnt = 2) THEN
-                        out_valid <= '1';
-                    ELSE
-                        out_valid <= '0';
+                    IF (cnt = 2) AND (out_valid_reg = '0') THEN
+                        out_valid_reg <= '1';
                     END IF;
                 END IF;
             END IF;

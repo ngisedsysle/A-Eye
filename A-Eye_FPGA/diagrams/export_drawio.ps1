d577a2bb8970d233d6d5173d33c7467568d16360
GuilhemROLLAND <guilhem99.rolland@gmail.com>
2022-07-28 16:08:20 +0200

    add dma architecture to documentation

---
+++ A-Eye_FPGA/diagrams/export_drawio.ps1
@@ -0,0 +1,2 @@
+& 'C:\Program Files\draw.io\draw.io.exe' -x '.\DMA data transfert for convolution.drawio' -f png -o out/archi_v2
+& 'C:\Program Files\draw.io\draw.io.exe' -x '.\adder_3_clk.drawio' -f png -o out/archi_v2

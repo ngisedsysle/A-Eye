8601d042ed6e6fdbbda2ac7743700d0891d1667e
Noemie Rolland <noemie.rolland@elsys-design.com>
2022-07-29 13:28:50 +0200

    Addition of the nex architecture diagram with axi stream fifo IP

--- A-Eye_FPGA/diagrams/export_drawio.ps1
+++ A-Eye_FPGA/diagrams/export_drawio.ps1
@@ -1,2 +1,3 @@
 & 'C:\Program Files\draw.io\draw.io.exe' -x '.\DMA data transfert for convolution.drawio' -f png -o out/archi_v2
 & 'C:\Program Files\draw.io\draw.io.exe' -x '.\adder_3_clk.drawio' -f png -o out/archi_v2
+& 'C:\Program Files\draw.io\draw.io.exe' -x '.\archi_conv_axi_fifo.drawio' -f png -o out/archi_v2
